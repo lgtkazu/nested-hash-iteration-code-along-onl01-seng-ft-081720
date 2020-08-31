9afb4881a602783847c3b95a93d2dccc9a8d1a08
Seth Mariscotes <seth.mariscotes@yahoo.com>
2020-08-31 01:06:18 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -9,17 +9,3 @@ def remove_strawberry(contacts)
   end
 end
 
-# Alternative solution
-
-def remove_strawberry(contacts)
-  # Assign Freddy Mercury's hash to a variable
-  freddy = contacts["Freddy Mercury"]
-
-  # Iterate over only Freddy Mercury's data
-  freddy.each do |attribute, value|
-    if attribute == :favorite_icecream_flavors
-      value.delete_if{ |ice_cream| ice_cream == "strawberry" }
-    end
-  end
-  contacts
-end
